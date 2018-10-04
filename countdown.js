bdf9dd72fbcc126b9dd052edaf49b142644539c1
18475573686 <39574204+18475573686@users.noreply.github.com>
2018-10-04 16:34:09 +0800

    定义屏幕和宽度和高度的全局变量

--- countdown.js
+++ countdown.js
@@ -1,9 +1,12 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
 window.onload = function () {
   /** @type HTMLCanvasElement */
   var canvas = this.document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
-
+  canvas.width = WINDOW_WIDTH;
+  canvas.height = WINDOW_HEIGHT;
 
   
 }
