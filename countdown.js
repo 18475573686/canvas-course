581660937ce13274389d370972be900768a28655
18475573686 <39574204+18475573686@users.noreply.github.com>
2018-10-04 16:51:44 +0800

    倒计时程序的基本架构

--- countdown.js
+++ countdown.js
@@ -8,5 +8,26 @@ window.onload = function () {
   canvas.width = WINDOW_WIDTH;
   canvas.height = WINDOW_HEIGHT;
 
+  render(ctx);
   
+}
+function render(ctx) { // 绘制canvas的画布
+  var hours = 12,
+      minutes = 34,
+      seconds = 56;
+  renderDigit(0, 0, parseInt(hours/10), ctx); // 渲染小时的第一个数字
+}
+
+function renderDigit(x, y, num, ctx) {
+  ctx.fillStyle = 'rgb(0, 102, 153)';
+  for (let i = 0; i < digit[num].length; i++) {
+    for (let j = 0; j < digit[num][i].length; j++) {
+      if (digit[num][i][j] == 1) { // 这里绘制圆球
+        ctx.beginPath();
+        ctx.arc();
+        ctx.closePath();
+        ctx.fill();
+      }
+    }
+  }
 }
